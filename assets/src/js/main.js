bb88b0e5fe6a4f92204b1834634b3d13a23da3d1
jyoti-wp <sim_s006@icloud.com>
2021-04-09 00:15:41 +0530

    header nav update

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,1 +1,15 @@
-import '../scss/main';
+import "../scss/main";
+
+// header js
+
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", newNav);
+
+function newNav {
+    if (window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('click')
+    } else {
+        nav.classList.remove('click')
+    }
+}
